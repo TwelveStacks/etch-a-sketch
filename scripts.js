6a7fef7b212d11ffda4b397ef3af3d55d6c8eb78
Ryan Chan <122183743+TwelveStacks@users.noreply.github.com>
2023-06-23 17:15:26 -0400

    Created 16x16 grid

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 const container = document.querySelector('.container');
 
-function createRow() {
+function createColumn() {
+    const squareDiv = document.createElement('div');
     for (let i = 0; i < 16; i++) {
         var square = document.createElement('div');
         square.id = 'square' + i;
         square.className = 'square';
         square.style.cssText = 'height: 50px; width: 50px;'
         square.style.border = '1px solid black';
-        container.appendChild(square)
+        squareDiv.appendChild(square);
     }
+    container.appendChild(squareDiv);
 }
 
 
 
-// for (let i = 0; i < 16; i++) {
-//     createRow();
-// 
-
-createRow();
+for (let i = 0; i < 16; i++) {
+    createColumn();
+}
 
